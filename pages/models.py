db9a2f3500b1bf2b716e67e2ee1df3ded92afe73
Arvin Visi <arvinvisi03@gmail.com>
2022-11-29 18:51:12 +0330

    add Article in model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='Article/photo')
+
+    datetime_create = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
