d7a7b40f2be26622cb0a8e27496c8c7286db812a
Arvin Visi <arvinvisi03@gmail.com>
2022-11-29 19:00:42 +0330

    add ArticleAdmin in admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'datetime_modified', )
+    search_fields = ('title', )
