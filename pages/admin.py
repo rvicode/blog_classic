bf8ef3ee9ead783f20f60020a3012fe0b555d198
Arvin Visi <arvinvisi03@gmail.com>
2022-12-08 09:57:16 +0330

    add Comment in admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Article, Category
+from .models import Article, Category, Comment
 
 
 @admin.register(Category)
@@ -13,3 +13,9 @@ class CategoryAdmin(admin.ModelAdmin):
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'datetime_modified', )
     search_fields = ('title', )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'article', 'parent', )
+    search_fields = ('username', 'article', 'parent', )
