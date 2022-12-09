5d5d943569d06b9337760a6ebae78ff7c5f6c330
Arvin Visi <arvinvisi03@gmail.com>
2022-12-09 18:25:09 +0330

    add AllAricleView and make all_post template

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('detail/<int:pk>', views.article_detail, name='detail_post'),
+    path('all', views.AllArticleView.as_view(), name='all_post'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
+from django.views.decorators.http import require_GET
 
 from .models import Article, Category, Comment
 
 
 class HomeView(generic.ListView):
-    model = Article
+    http_method_names = ['get']
+    queryset = Article.objects.all()[:6]
     template_name = 'pages/home.html'
     context_object_name = 'article'
-    paginate_by = 6
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
@@ -16,6 +17,15 @@ class HomeView(generic.ListView):
         return context
 
 
+class AllArticleView(generic.ListView):
+    http_method_names = ['get']
+    model = Article
+    template_name = 'pages/all_post.html'
+    context_object_name = 'article'
+    paginate_by = 1
+
+
+@require_GET
 def article_detail(request, pk):
     article = get_object_or_404(Article, id=pk)
     category = Category.objects.all()[:10]
