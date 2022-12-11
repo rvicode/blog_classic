b7bbe167d476050fc1e839ec55bd600a97815ccd
Arvin Visi <arvinvisi03@gmail.com>
2022-12-11 17:31:21 +0330

    add login and signup in template

--- pages/urls.py
+++ pages/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('detail/<int:pk>', views.article_detail, name='detail_post'),
     path('all', views.AllArticleView.as_view(), name='all_post'),
-    path('all/<int:pk>', views.article_category, name='all_post_with_category'),
+    path('all/<int:pk>', views.article_category_view, name='all_post_with_category'),
 ]
