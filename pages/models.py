cad4f8e5c4a170dbb435a9a616b32c7ecc6529a3
Arvin Visi <arvinvisi03@gmail.com>
2022-12-11 17:14:55 +0330

    add article_category_view

--- pages/models.py
+++ pages/models.py
@@ -13,6 +13,9 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('pages:all_post_with_category', args=[self.id])
+
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 from .models import Article, Category, Comment
 
@@ -25,13 +26,24 @@ class AllArticleView(generic.ListView):
     paginate_by = 6
 
 
+def article_category_view(request, pk):
+    category = get_object_or_404(Category, id=pk)
+    article = Article.objects.filter(category=category)
+    paginator = Paginator(article, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'pages/all_post.html', {'article': page_obj})
+
+
 def article_detail(request, pk):
     article = get_object_or_404(Article, id=pk)
     category = Category.objects.all()[:10]
     articles = Article.objects.all().order_by('-datetime_create')[:3]
+
     if request.method == 'POST':
         massage = request.POST.get('message')
         parent_id = request.POST.get('parent_id')
+
         Comment.objects.create(article=article, username=request.user, massage=massage, parent_id=parent_id)
         messages.success(request, 'Your comment was successfully ! ')
     return render(request, 'pages/detail_post.html',
